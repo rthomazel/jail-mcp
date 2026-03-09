5e4cf9d54a9da6c83551d85331f4e78791f6e287
Raphael "Thom" Thomazella <thomazella9@gmail.com>
2026-03-09 12:43:35 -0300

    misc: format

--- handlers/exec_sync.go
+++ handlers/exec_sync.go
@@ -15,10 +15,10 @@ import (
 )
 
 type commandResult struct {
-	Stdout   string        `json:"stdout"`
-	Stderr   string        `json:"stderr"`
-	ExitCode int           `json:"exit_code"`
-	Duration string        `json:"duration"`
+	Stdout   string `json:"stdout"`
+	Stderr   string `json:"stderr"`
+	ExitCode int    `json:"exit_code"`
+	Duration string `json:"duration"`
 	err      string
 }
 
